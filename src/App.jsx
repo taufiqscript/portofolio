968764f78b498fe598ab3fe40313861c0ccb7536
unknown <28.taufiqurrahman@gmail.com>
2025-10-28 14:34:51 +0700

    add Mata Air Nusantara to portofolio

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,27 @@ import deved from "/dev-ed-wave.png";
 import responsif from "/front-end-programming.png";
 import fullstack from "/thorough.png";
 import api from "/api-integration.png";
-import video1 from '/video-1.mp4'
-import video2 from '/video-2.mp4'
-import video3 from '/video-3.mp4'
-import video4 from '/video-4.mp4'
-import ecommerce1 from '/ecommerce/ecommerce-1.mp4'
-import ecommerce2 from '/ecommerce/ecommerce-2.mp4'
+import video1 from '/netflix/video-1.webm'
+import video2 from '/netflix/video-2.webm'
+import video3 from '/netflix/video-3.webm'
+import video4 from '/netflix/video-4.webm'
+import ecommerce1 from '/ecommerce/ecommerce-1.webm'
+import ecommerce2 from '/ecommerce/ecommerce-2.webm'
+import mataAirVideo from '/mataairnusantara/mataairnusantara.webm'
 
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(false)
   const videos = [video1, video2, video3, video4]
   const ecommerce = [ecommerce1, ecommerce2]
+  const mataairnusantara = [mataAirVideo]
   const [contentIndex, setContentIndex] = useState(0)
   const [contentEcommerceIndex, setContentEcommerceIndex] = useState(0)
   const [fade, setFade] = useState(true)
   const [fadeEcommerce, setFadeEcommerce] = useState(true)
   const videoRefNetflix = useRef(null)
   const videoRefEcommerce = useRef(null)
+  const videoRefMataAir = useRef(null)
 
   useEffect(() => {
     if (videoRefNetflix.current) {
@@ -40,6 +43,13 @@ const App = () => {
       videoRefEcommerce.current.play()
     }
   }, [contentEcommerceIndex])
+
+  useEffect(() => {
+    if (videoRefMataAir.current) {
+      videoRefMataAir.current.load()
+      videoRefMataAir.current.play()
+    }
+  }, [])
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -85,7 +95,7 @@ const App = () => {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white sm:px-4 px-2 sm:py-2 py-1 border-none rounded-md sm:text-[16px] text-[10px]"
-                  href="#"
+                  href="#portofolio"
                 >
                   Resume
                 </a>
@@ -103,10 +113,10 @@ const App = () => {
               A graduate in Computer Science with a strong interest in full-stack web development. Experienced in building projects using the MERN Stack, including CRUD applications and responsive websites. Able to work both independently and in a team, with a fast ability to adapt to new technologies.
             </p>
             <div className="sm:text-5xl text-[24px] flex justify-center sm:gap-4 gap-2 sm:py-3 py-1.5 text-gray-600 dark:text-gray-400">
-              <a href="https://github.com/taufiqscript">
+              <a target="_blank" href="https://github.com/taufiqscript">
                 <AiFillGithub className="cursor-pointer" />
               </a>
-              <a href="https://www.linkedin.com/in/taufiq-rahman-98a322356">
+              <a target="_blank" href="https://www.linkedin.com/in/taufiq-rahman-98a322356">
                 <AiFillLinkedin
                   className="cursor-pointer"
                 />
@@ -115,6 +125,7 @@ const App = () => {
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full sm:w-80 w-40 sm:h-80 h-40 relative overflow-hidden sm:mt-20 mt-10 md:h-96 md:w-96">
               <img
                 src={deved}
+                loading="lazy"
                 width={"100%"}
                 height={"100%"}
                 className="object-cover w-full h-full" />
@@ -137,6 +148,7 @@ const App = () => {
             <div className="text-center shadow-lg sm:p-10 p-5 rounded-xl my-10 dark:bg-white flex-1">
               <img
                 src={fullstack}
+                loading="lazy"
                 className="sm:w-[100px] w-[60px] sm:h-[100px] h-[60px]"
               />
               <h3 className="text-lg font-medium pt-8 pb-2 ">
@@ -154,6 +166,7 @@ const App = () => {
             <div className="text-center shadow-lg sm:p-10 p-5 rounded-xl my-10 dark:bg-white flex-1">
               <img
                 src={responsif}
+                loading="lazy"
                 className="sm:w-[100px] w-[60px] sm:h-[100px] h-[60px]"
               />
               <h3 className="text-lg font-medium pt-8 pb-2">
@@ -172,6 +185,7 @@ const App = () => {
             <div className="text-center shadow-lg sm:p-10 p-5 rounded-xl my-10 dark:bg-white flex-1">
               <img
                 src={api}
+                loading="lazy"
                 className="sm:w-[100px] w-[65px] sm:h-[100px] h-[65px]"
               />
               <h3 className="text-lg font-medium pt-8 pb-2">API Development & Integration</h3>
@@ -185,7 +199,7 @@ const App = () => {
             </div>
           </div>
         </section>
-        <section className="sm:py-10 py-5">
+        <section id="portofolio" className="sm:py-10 py-5">
           <h3 className="sm:text-3xl text-[18px] sm:py-1 py-0.5 dark:text-white ">Portofolio</h3>
           <p className="sm:text-[16px] text-[11px] sm:py-2 py-1 sm:leading-8 leading-6 text-gray-800 dark:text-gray-200">
             As a <span className="text-teal-500">Fullstack Developer</span> experienced in web development using the <span className="text-teal-500">MERN Stack</span>, I have built various projects focusing on interactive user interfaces and optimal application performance.
@@ -196,11 +210,14 @@ const App = () => {
           <div className="flex flex-col sm:gap-10 gap-5 sm:py-10 py-5 lg:flex-row lg:flex-wrap">
             <div className="flex sm:gap-4 gap-2 flex-col max-w-5xl">
               <div>
-                <h3 className="font-bold sm:text-xl text-[16px] text-black dark:text-white">Netflix Clone | MERN Stack</h3>
-                <p className="dark:text-white sm:text-[16px] text-[10px]">
-                  A streaming web application replicating core Netflix features, complete with user authentication, favorites list, and trailer previews.
+                <h3 className="font-bold sm:text-xl text-[16px] text-black dark:text-white">🎬 Netflix Clone | MERN Stack</h3>
+                <p className="dark:text-white sm:text-[16px] text-justify text-[10px]">
+                  A full-featured streaming web application inspired by Netflix, developed using the <span className="text-teal-500">MERN Stack</span>.
+                  The project includes user authentication, personalized favorites list, and real-time trailer previews.
+                  Built with responsive design and optimized performance to deliver an immersive viewing experience.
                   <a
                     href="https://netflix-clone-mern-eta.vercel.app"
+                    target="_blank"
                     className="text-blue-400 hover:text-blue-600"
                   > Link Demo</a>
                 </p>
@@ -221,15 +238,17 @@ const App = () => {
             </div>
             <div className="flex sm:gap-4 gap-2 flex-col max-w-5xl">
               <div>
-                <h3 className="font-bold sm:text-xl text-[16px] text-black dark:text-white">EcoMart | E-commerce Website</h3>
-                <p className="dark:text-white sm:text-[16px] text-[10px]">
-                  EcoMart is a modern e-commerce application built with React.js (Vite) on the frontend and Express.js with MongoDB on the backend.
-                  This project is designed to simulate an online shopping experience similar to major marketplaces such as Shopee or Tokopedia. 
+                <h3 className="font-bold sm:text-xl text-[16px] text-black dark:text-white">🛒 EcoMart | E-commerce Website</h3>
+                <p className="dark:text-white sm:text-[16px] text-justify text-[10px]">
+                  <span className="text-teal-500">EcoMart</span> is a modern e-commerce web application designed to simulate an online marketplace experience similar to Shopee or Tokopedia.
+                  Developed with <span className="text-teal-500">React.js (Vite)</span> on the frontend and <span className="text-teal-500">Express.js + MongoDB</span> on the backend,
+                  it features product management, shopping cart functionality, user authentication, and a fully responsive interface optimized for desktop and mobile. 
                   <a
                     href="https://ecommerce-project-client-two.vercel.app/"
+                    target="_blank"
                     className="text-blue-400 hover:text-blue-600 sm:text-[16px] text-[8px]"
                   >
-                    <br />Link Demo
+                    <span> Link Demo</span>
                   </a>
                 </p>
               </div>
@@ -243,7 +262,38 @@ const App = () => {
                   height={"100%"}
                   className={`absolute top-0 left-0 object-cover transition-opacity duration-[800ms] w-full h-full ${fadeEcommerce ? 'opacity-100' : 'opacity-0'}`}
                 >
-                  <source src={ecommerce[contentEcommerceIndex]} type="video/mp4" />
+                  <source src={ecommerce[contentEcommerceIndex]} type="video/webm" />
+                </video>
+              </div>
+            </div>
+
+            <div className="flex sm:gap-4 gap-2 flex-col max-w-5xl">
+              <div>
+                <h3 className="font-bold sm:text-xl text-[16px] text-black dark:text-white">🌊 Mata Air Nusantara | Water Drilling Service Website</h3>
+                <p className="dark:text-white sm:text-[16px] text-justify text-[10px]">
+                  <span className="text-teal-500">Mata Air Nusantara</span> is a professional water drilling service website focused on the Bekasi, Jakarta, and surrounding areas.
+                  Built using <sapn className="text-teal-500">React.js</sapn> and <span className="text-teal-500">Framer Motion</span> for smooth animations, this modern landing page is optimized for <span className="text-teal-500">SEO, includes schema.org structured data,</span> and features an <span className="text-teal-500">automated WhatsApp form</span> to make it easier for potential clients to get in touch.
+                  Key features include a responsive navigation system, interactive pricing list, customer testimonials, educational blog posts, and seamless integration with Google Maps and WhatsApp API for instant communication.
+                  <a
+                    href="https://www.mataairnusantara.com/"
+                    target="_blank"
+                    className="text-blue-400 hover:text-blue-600 sm:text-[16px] text-[8px]"
+                  >
+                    <span> Link Demo</span>
+                  </a>
+                </p>
+              </div>
+              <div className="relative sm:w-[1000px] w-[320px] sm:h-[80vh] h-[30vh] overflow-hidden rounded-lg">
+                <video
+                  ref={videoRefMataAir}
+                  autoPlay
+                  loop
+                  muted
+                  width={"100%"}
+                  height={"100%"}
+                  className={`absolute top-0 left-0 object-cover w-full h-full`}
+                >
+                  <source src={mataairnusantara[0]} type="video/webm" />
                 </video>
               </div>
             </div>
